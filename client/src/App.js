9cb560de620e2ae1780f633ea0909dc7fb6b900d
Boss The Greatest MacBook <vlad@bosstheatestmbp.home>
2020-09-17 15:12:10 +0100

    Modified file

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React or don't
           </a>
       </div>
     </Router>
